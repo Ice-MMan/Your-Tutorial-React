aa34db6779709ef5aecf42ddf0dbf130d5d092ee
Kaan UYSAL <kaanuysal06@gmail.com>
2022-10-20 02:25:34 +0300

    first commit

---
+++ src/components/EditTutorial.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+
+const EditTutorial = ({ sendEdit, edit, putedit }) => {
+  const { id, title, description } = edit;
+
+  const [changetitle, setChangetitle] = useState("");
+  const [changedesc, setChangedesc] = useState("");
+
+  console.log(changetitle, changedesc);
+
+  const changetutor = (e) => {
+    e.preventDefault();
+    sendEdit(id, changetitle, changedesc);
+    putedit(id, changetitle, changedesc);
+  };
+
+  console.log(edit);
+  return (
+    <div>
+      <div className="modal fade" id="edit-modal" tabIndex={-1}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Edit Tutorial
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              />
+            </div>
+            <div className="modal-body">
+              <form onSubmit={changetutor}>
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">
+                    Title
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="title"
+                    placeholder={title}
+                    // value={title}
+                    onChange={(e) => setChangetitle(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="desc" className="form-label">
+                    Description
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="desc"
+                    placeholder={description}
+                    // value={description}
+                    onChange={(e) => setChangedesc(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="modal-footer">
+                  <button
+                    type="button"
+                    className="btn btn-secondary"
+                    data-bs-dismiss="modal"
+                  >
+                    Close
+                  </button>
+                  <button
+                    type="submit"
+                    data-bs-dismiss="modal"
+                    className="btn btn-primary"
+                  >
+                    Save changes
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditTutorial;
---
+++ src/components/TutorialList.jsx
@@ -0,0 +1,90 @@
+import { FaEdit } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import axios from "axios";
+import EditTutorial from "./EditTutorial";
+import { useState } from "react";
+
+const TutorialList = ({ tutorials, getTutorials }) => {
+  const [edit, setEdit] = useState({ id: "", title: "", description: "" });
+
+  const deleteTutorial = async (id) => {
+    const url = `https://axios-example-cw.herokuapp.com/api/tutorials/${id}`;
+
+    try {
+      await axios.delete(url);
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+
+  const sendEdit = (id, title, description) => {
+    setEdit({ id: id, title: title, description: description });
+    putedit(id, title, description);
+
+    console.log(id);
+  };
+
+  const putedit = async (id, title, description) => {
+    const url = "https://axios-example-cw.herokuapp.com/api/tutorials";
+
+    try {
+      // console.log("puta girdi");
+      // console.log(title);
+      await axios.put(`${url}/${id}`, { title, description });
+    } catch (error) {
+      console.log(error);
+    }
+    getTutorials();
+  };
+
+  // console.log(edit);
+
+  return (
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials?.map((item) => {
+            const { id, title, description } = item;
+            return (
+              <tr key={id}>
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    size={20}
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#edit-modal"
+                    className="me-2 text-warning"
+                    onClick={() => sendEdit(id, title, description)}
+                  />
+                  <AiFillDelete
+                    size={22}
+                    type="button"
+                    className="text-danger "
+                    onClick={() => deleteTutorial(id)}
+                  />
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+      <EditTutorial sendEdit={sendEdit} edit={edit} putedit={putedit} />
+    </div>
+  );
+};
+
+export default TutorialList;
